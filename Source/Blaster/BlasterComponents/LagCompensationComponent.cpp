17c59e47724d7f9506bc2412ddca620c71ad2d97
midoli <936184713@qq.com>
2025-08-23 17:03:31 +0800

    188 创建了延迟补偿的组件

---
+++ Source/Blaster/BlasterComponents/LagCompensationComponent.cpp
@@ -0,0 +1,22 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "LagCompensationComponent.h"#include "LagCompensationComponent.h"
+
+ULagCompensationComponent::ULagCompensationComponent()ULagCompensationComponent::ULagCompensationComponent()
+{
+	PrimaryComponentTick.bCanEverTick = true;
+
+}
+
+void ULagCompensationComponent::BeginPlay()void ULagCompensationComponent::BeginPlay()
+{
+	Super::BeginPlay();
+
+}
+
+void ULagCompensationComponent::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)void ULagCompensationComponent::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+
+}
