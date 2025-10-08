6a1d906b3984b17ef4ab21edbcfcdb3135fe20d3
midoli <936184713@qq.com>
2025-10-08 16:43:11 +0800

    224 队伍游戏模式

---
+++ Source/Blaster/GameMode/TeamsGameMode.cpp
@@ -0,0 +1,92 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "TeamsGameMode.h"
+#include "Blaster/GameState/BlasterGameState.h"
+#include "Blaster/PlayerState/BlasterPlayerState.h"
+#include "Kismet/GameplayStatics.h"
+
+/// <summary>
+/// 玩家连接游戏进入房间
+/// 在一位新玩家完全登录并准备好进入游戏世界之后。这发生在玩家控制器被创建、初始化并与游戏世界关联之后，比 BeginPlay 更晚。
+/// </summary>
+/// <param name="NewPlayer"></param>
+void ATeamsGameMode::PostLogin(APlayerController* NewPlayer)
+{
+	Super::PostLogin(NewPlayer);
+
+	ABlasterGameState* BGameState = Cast<ABlasterGameState>(UGameplayStatics::GetGameState(this));
+	if (BGameState)
+	{
+		ABlasterPlayerState* BPState = NewPlayer->GetPlayerState<ABlasterPlayerState>();
+		if (BPState && BPState->GetTeam() == ETeam::ET_NoTeam)
+		{
+			//玩家连接房间时，依据两边队伍人数分配到人少的队伍中
+			if (BGameState->BlueTeam.Num() >= BGameState->RedTeam.Num())
+			{
+				BGameState->RedTeam.AddUnique(BPState);
+				BPState->SetTeam(ETeam::ET_RedTeam);
+			}
+			else
+			{
+				BGameState->BlueTeam.AddUnique(BPState);
+				BPState->SetTeam(ETeam::ET_BlueTeam);
+			}
+		}
+	}
+}
+
+/// <summary>
+/// 玩家断连
+/// 在一位玩家的控制器（AController）即将被从世界中销毁之前。这发生在玩家主动断开连接、网络中断或服务器将其踢出时。
+/// </summary>
+/// <param name="Exiting"></param>
+void ATeamsGameMode::Logout(AController* Exiting)
+{
+	ABlasterGameState* BGameState = Cast<ABlasterGameState>(UGameplayStatics::GetGameState(this));
+	ABlasterPlayerState* BPState = Exiting->GetPlayerState<ABlasterPlayerState>();
+	//从队伍中删除玩家
+	if (BGameState && BPState)
+	{
+		if (BGameState->RedTeam.Contains(BPState))
+		{
+			BGameState->RedTeam.Remove(BPState);
+		}
+		if (BGameState->BlueTeam.Contains(BPState))
+		{
+			BGameState->BlueTeam.Remove(BPState);
+		}
+	}
+
+}
+
+/// <summary>
+/// 当比赛（Match）正式开始时。这个调用是由游戏框架内部触发的，通常在你调用 StartMatch() 之后发生。
+/// </summary>
+void ATeamsGameMode::HandleMatchHasStarted()
+{
+	Super::HandleMatchHasStarted();
+
+	ABlasterGameState* BGameState = Cast<ABlasterGameState>(UGameplayStatics::GetGameState(this));
+	if (BGameState)
+	{
+		//遍历所有玩家进行队伍分配
+		for (auto PState : BGameState->PlayerArray)
+		{
+			ABlasterPlayerState* BPState = Cast<ABlasterPlayerState>(PState.Get());
+			if (BPState && BPState->GetTeam() == ETeam::ET_NoTeam)
+			{
+				if (BGameState->BlueTeam.Num() >= BGameState->RedTeam.Num())
+				{
+					BGameState->RedTeam.AddUnique(BPState);
+					BPState->SetTeam(ETeam::ET_RedTeam);
+				}
+				else
+				{
+					BGameState->BlueTeam.AddUnique(BPState);
+					BPState->SetTeam(ETeam::ET_BlueTeam);
+				}
+			}
+		}
+	}
+}
---
+++ Source/Blaster/GameMode/TeamsGameMode.h
@@ -0,0 +1,33 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "BlasterGameMode.h"
+#include "TeamsGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BLASTER_API ATeamsGameMode : public ABlasterGameMode
+{
+	GENERATED_BODY()
+public:
+	/// <summary>
+	/// 玩家连接游戏进入房间
+	/// 在一位新玩家完全登录并准备好进入游戏世界之后。这发生在玩家控制器被创建、初始化并与游戏世界关联之后，比 BeginPlay 更晚。
+	/// </summary>
+	virtual void PostLogin(APlayerController* NewPlayer) override;
+	/// <summary>
+	/// 玩家断连
+	/// 在一位玩家的控制器（AController）即将被从世界中销毁之前。这发生在玩家主动断开连接、网络中断或服务器将其踢出时。
+	/// </summary>
+	/// <param name="Exiting"></param>
+	virtual void Logout(AController* Exiting) override;
+protected:
+	/// <summary>
+	/// 当比赛（Match）正式开始时。这个调用是由游戏框架内部触发的，通常在你调用 StartMatch() 之后发生。
+	/// </summary>
+	virtual void HandleMatchHasStarted() override;
+};
